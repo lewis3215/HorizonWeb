9648ab4ad6d77602e013580d32aee6e713991df2
Arsène Lapostolet <arsene@lapostolet.fr>
2021-11-22 13:28:01 +0100

    implement interface

--- api/src/teams/service/team.service.default.ts
+++ api/src/teams/service/team.service.default.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import type { CreateOrUpdateTeamDto } from './dto/create-team.dto';
+import {CreateOrUpdateTeamMember} from "./dto/create-team-member.dto";
+import type { CreateTeamDto } from './dto/create-team.dto';
+import {GetTeamWithMembersDto} from "./dto/get-team-with-members.dto";
 import type { GetTeamDto } from './dto/get-team.dto';
+import {UpdateTeamDto} from "./dto/update-team.dto";
 import { TeamRepository } from './team.repository';
 import type { TeamService } from './team.service';
 
@@ -12,7 +15,27 @@ export class DefaultTeamService implements TeamService {
         this.repository = repository;
     }
 
-    public createTeam(dto: CreateOrUpdateTeamDto): GetTeamDto {
+    public createTeam(dto: CreateTeamDto): GetTeamDto {
+        return undefined;
+    }
+
+    public addMember(teamId: string, currentUserId: string, member: CreateOrUpdateTeamMember): void {
+    }
+
+    public delete(teamId: string, userId: string, currentUserId: string): void {
+    }
+
+    public deleteTeam(teamId: string, currentUserId: string): void {
+    }
+
+    public getTeam(teamId: string, currentUserId: string): GetTeamWithMembersDto {
+        return undefined;
+    }
+
+    public updateRole(teamId: string, currentUserId: string, member: CreateOrUpdateTeamMember): void {
+    }
+
+    public updateTeam(teamId: string, currentUserId: string, team: UpdateTeamDto): GetTeamDto {
         return undefined;
     }
 }
