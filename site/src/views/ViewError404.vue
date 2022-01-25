c8233de396bbbd8570e800c8eedb868f372b99aa
Unaxe <74629449+Unaxe@users.noreply.github.com>
2022-01-25 18:09:48 +0100

    Start 404

---
+++ site/src/views/ViewError404.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="relative w-full h-screen">
+        <div class="overflow-hidden absolute z-10 w-full h-full "><img class="w-full" src="https://i.imgur.com/nY1SVr7.gif?noredirect"></div>
+        <div class="flex absolute inset-x-0 z-20 flex-col ml-32 w-1/2 h-full">
+            <div class="mt-auto text-9xl font-bold text-white whitespace-nowrap">404</div>
+            <div class="text-7xl font-bold text-white whitespace-nowrap">NOT FOUND</div>
+            <div class="mb-auto text-xl text-white">Vous êtes parti un peu loin, revenez parmi nous</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {}
+</script>
+<style>
+
+</style>
