c3990ee4832f6f156be406293ae51963f650ae03
lewis3215 <juniordjomo07@gmail.com>
2022-01-29 10:19:30 +0100

    git

---
+++ site/src/components/Card/ContactsCard.vue
@@ -0,0 +1,107 @@
+<template>
+  <div :class="id">
+    <div class="card">
+      <div class="card-high">
+        <img :src="url" />
+      </div>
+      <div class="card-low">
+        <h1>{{ nom }}</h1>
+        <h3>{{poste}}</h3>
+       <div>
+          <p>Adresse </p>
+           <b> {{email}} </b>
+       </div>
+       <div>
+          <p>Tel </p>
+           <b> {{numero}} </b>
+       </div>
+       <div>
+          <p>Linkedink </p>
+           <b> {{link}} </b>
+       </div>
+         
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactsCard',
+  props: ["nom","url", "email", "poste", "numero", "link"],
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+
+<style scoped>
+@media (min-width: 700px) {
+  .card {
+    margin: 20px;
+    box-shadow: 0px 5px 10px #ccc;
+    background-color: white;
+    display: grid;
+    grid-template-columns:  1fr;
+  }
+}
+@media (max-width: 699px) {
+  .card {
+    margin: 20px;
+    box-shadow: 0px 5px 10px #ccc;
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+}
+.card-high img {
+  width: 70%;
+  margin: auto;
+  height: 80%;
+  max-height: 300px;
+  border-radius: 70%;
+}
+.card-low {
+  text-align: center;
+  position: relative;
+  margin: 20px;
+  display: grid;
+}
+.infos p {
+  display: flex;
+}
+a {
+  color: var(--first-color);
+  text-decoration: none;
+  font-weight: bold;
+  
+  display: inline-block;
+  padding: 15px 20px;
+  position: relative;
+}
+a:after {    
+  background: none repeat scroll 0 0 transparent;
+  bottom: 0;
+  content: "";
+  display: block;
+  height: 2px;
+  left: 50%;
+  position: absolute;
+  background: var(--first-color);
+  transition: width 0.3s ease 0s, left 0.3s ease 0s;
+  width: 0;
+}
+a:hover:after { 
+  width: 100%; 
+  left: 0; 
+}
+
+p{
+  float: left;
+}
+
+b{
+  float: right;
+}
+</style>
--- site/src/views/Guide/GuideContacts.vue
+++ site/src/views/Guide/GuideContacts.vue
@@ -1,5 +1,6 @@
-<template lang="">
-    <!-- component -->
+<template lang="" >
+  <div  class="sep">
+      <!-- component -->
     <!-- MDI Icons -->
     <link
         rel="stylesheet"
@@ -7,261 +8,92 @@
     >
 
     <!-- Page Container -->
-    <div class="flex items-center justify-center min-h-screen bg-white py-48">
-        <div class="flex flex-col">
-            <div class="flex flex-col mt-8">
-                <!-- Meet the Team -->
-                <div class="container max-w-7xl px-4">
-                    <!-- Section Header -->
-                    <div class="flex flex-wrap justify-center text-center mb-24">
-                        <div class="w-full lg:w-6/12 px-4">
-                            <!-- Header -->
-                            <h1 class="text-gray-900 text-4xl font-bold mb-8">
-                                Meet the Team
-                            </h1>
-
-                            <!-- Description -->
-                            <p class="text-gray-700 text-lg font-light">
-                                With over 100 years of combined experience, we've got a well-seasoned team at the helm.
-                            </p>
-                        </div>
-                    </div>
-
-                    <!-- Team Members -->
-                    <div class="flex flex-wrap">
-                        <!-- Member #1 -->
-                        <div class="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
-                            <div class="flex flex-col">
-                                <!-- Avatar -->
-                                <a href="#">
-                                    <img
-                                        class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
-                                        src="https://images.unsplash.com/photo-1634926878768-2a5b3c42f139?fit=clamp&w=400&h=400&q=80"
-                                    >
-                                </a>
-
-                                <!-- Details -->
-                                <div class="text-center mt-6">
-                                    <!-- Name -->
-                                    <h1 class="text-gray-900 text-xl font-bold mb-1">
-                                        Tranter Jaskulski
-                                    </h1>
-
-                                    <!-- Title -->
-                                    <div class="text-gray-700 font-light mb-2">
-                                        Founder & Specialist
-                                    </div>
-
-                                    <!-- Social Icons -->
-                                    <div
-                                        class="flex items-center justify-center opacity-50 hover:opacity-100
-                                    transition-opacity duration-300"
-                                    >
-                                        <!-- Linkedin -->
-                                        <a
-                                            href="#"
-                                            class="flex rounded-full hover:bg-indigo-50 h-10 w-10"
-                                        >
-                                            <i class="mdi mdi-linkedin text-indigo-500 mx-auto mt-2" />
-                                        </a>
-
-                                        <!-- Twitter -->
-                                        <a
-                                            href="#"
-                                            class="flex rounded-full hover:bg-blue-50 h-10 w-10"
-                                        >
-                                            <i class="mdi mdi-twitter text-blue-300 mx-auto mt-2" />
-                                        </a>
-
-                                        <!-- Instagram -->
-                                        <a
-                                            href="#"
-                                            class="flex rounded-full hover:bg-orange-50 h-10 w-10"
-                                        >
-                                            <i class="mdi mdi-instagram text-orange-400 mx-auto mt-2" />
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Member #2 -->
-                        <div class="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
-                            <div class="flex flex-col">
-                                <!-- Avatar -->
-                                <a href="#">
-                                    <img
-                                        class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
-                                        src="https://images.unsplash.com/photo-1634896941598-b6b500a502a7?fit=clamp&w=400&h=400&q=80"
-                                    >
-                                </a>
-
-                                <!-- Details -->
-                                <div class="text-center mt-6">
-                                    <!-- Name -->
-                                    <h1 class="text-gray-900 text-xl font-bold mb-1">
-                                        Denice Jagna
-                                    </h1>
-
-                                    <!-- Title -->
-                                    <div class="text-gray-700 font-light mb-2">
-                                        Tired & M. Specialist
-                                    </div>
-
-                                    <!-- Social Icons -->
-                                    <div
-                                        class="flex items-center justify-center opacity-50 hover:opacity-100
-                                    transition-opacity duration-300"
-                                    >
-                                        <!-- Linkedin -->
-                                        <a
-                                            href="#"
-                                            class="flex rounded-full hover:bg-indigo-50 h-10 w-10"
-                                        >
-                                            <i class="mdi mdi-linkedin text-indigo-700 mx-auto mt-2" />
-                                        </a>
-
-                                        <!-- Twitter -->
-                                        <a
-                                            href="#"
-                                            class="flex rounded-full hover:bg-blue-50 h-10 w-10"
-                                        >
-                                            <i class="mdi mdi-twitter text-blue-400 mx-auto mt-2" />
-                                        </a>
-
-                                        <!-- Instagram -->
-                                        <a
-                                            href="#"
-                                            class="flex rounded-full hover:bg-orange-50 h-10 w-10"
-                                        >
-                                            <i class="mdi mdi-instagram text-orange-400 mx-auto mt-2" />
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Member #3 -->
-                        <div class="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
-                            <div class="flex flex-col">
-                                <!-- Avatar -->
-                                <a href="#">
-                                    <img
-                                        class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
-                                        src="https://images.unsplash.com/photo-1634193295627-1cdddf751ebf?fit=clamp&w=400&h=400&q=80"
-                                    >
-                                </a>
-
-                                <!-- Details -->
-                                <div class="text-center mt-6">
-                                    <!-- Name -->
-                                    <h1 class="text-gray-900 text-xl font-bold mb-1">
-                                        Kenji Milton
-                                    </h1>
-
-                                    <!-- Title -->
-                                    <div class="text-gray-700 font-light mb-2">
-                                        Team Memeber
-                                    </div>
-
-                                    <!-- Social Icons -->
-                                    <div
-                                        class="flex items-center justify-center opacity-50 hover:opacity-100
-                                    transition-opacity duration-300"
-                                    >
-                                        <!-- Linkedin -->
-                                        <a
-                                            href="#"
-                                            class="flex rounded-full hover:bg-indigo-50 h-10 w-10"
-                                        >
-                                            <i class="mdi mdi-linkedin text-indigo-700 mx-auto mt-2" />
-                                        </a>
-
-                                        <!-- Twitter -->
-                                        <a
-                                            href="#"
-                                            class="flex rounded-full hover:bg-blue-50 h-10 w-10"
-                                        >
-                                            <i class="mdi mdi-twitter text-blue-400 mx-auto mt-2" />
-                                        </a>
-
-                                        <!-- Instagram -->
-                                        <a
-                                            href="#"
-                                            class="flex rounded-full hover:bg-orange-50 h-10 w-10"
-                                        >
-                                            <i class="mdi mdi-instagram text-orange-400 mx-auto mt-2" />
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Member #4 -->
-                        <div class="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
-                            <div class="flex flex-col">
-                                <!-- Avatar -->
-                                <a href="#">
-                                    <img
-                                        class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
-                                        src="https://images.unsplash.com/photo-1635003913011-95971abba560?fit=clamp&w=400&h=400&q=80"
-                                    >
-                                </a>
-
-                                <!-- Details -->
-                                <div class="text-center mt-6">
-                                    <!-- Name -->
-                                    <h1 class="text-gray-900 text-xl font-bold mb-1">
-                                        Doesn't matter
-                                    </h1>
-
-                                    <!-- Title -->
-                                    <div class="text-gray-700 font-light mb-2">
-                                        Will be fired
-                                    </div>
-
-                                    <!-- Social Icons -->
-                                    <div
-                                        class="flex items-center justify-center opacity-50 hover:opacity-100
-                                    transition-opacity duration-300"
-                                    >
-                                        <!-- Linkedin -->
-                                        <a
-                                            href="#"
-                                            class="flex rounded-full hover:bg-indigo-50 h-10 w-10"
-                                        >
-                                            <i class="mdi mdi-linkedin text-indigo-700 mx-auto mt-2" />
-                                        </a>
-
-                                        <!-- Twitter -->
-                                        <a
-                                            href="#"
-                                            class="flex rounded-full hover:bg-blue-50 h-10 w-10"
-                                        >
-                                            <i class="mdi mdi-twitter text-blue-400 mx-auto mt-2" />
-                                        </a>
-
-                                        <!-- Instagram -->
-                                        <a
-                                            href="#"
-                                            class="flex rounded-full hover:bg-orange-50 h-10 w-10"
-                                        >
-                                            <i class="mdi mdi-instagram text-orange-400 mx-auto mt-2" />
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <template v-for="todo in this.todos" :key="todo.nom">
+        <contactsCard  
+                :nom="todo.nom" 
+                :url="todo.url" 
+                :email="todo.email"
+                :poste="todo.poste"
+                :link="todo.link"
+                :numero="todo.numero"> 
+  </contactsCard>
+    </template>
+  </div>
 </template>
 <script>
+import ContactsCard from "@/components/Card/ContactsCard.vue";
+import img from "@/assets/img/landing/girl_1.jpg";
+
 export default {
-    name: 'GuideContactPage',
-}
+  name: "GuideContacts",
+  components: {
+    ContactsCard,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          nom: "lewis",
+          url: img,
+          nom: "jin",
+          email: "jin@gmail.com",
+          poste: "ifhsj",
+          numero: '0745316454',
+          link: "jdnsjcs",
+        },
+        {
+          nom: "lewis",
+          url: img,
+          nom: "jin",
+          email: "jin@gmail.com",
+          poste: "ifhsj",
+          numero: '0745316454',
+          link: "jdnsjcs",
+        },
+        {
+          nom: "lewis",
+          url: img,
+          nom: "jin",
+          email: "jin@gmail.com",
+          poste: "ifhsj",
+          numero: '0745316454',
+          link: "jdnsjcs",
+        },
+        {
+          nom: "lewis",
+          url: img,
+          nom: "jin",
+          email: "jin@gmail.com",
+          poste: "ifhsj",
+          numero: '0745316454',
+          link: "jdnsjcs",
+        },
+        {
+          nom: "lewis",
+          url: img,
+          nom: "jin",
+          email: "jin@gmail.com",
+          poste: "ifhsj",
+          numero: '0745316454',
+          link: "jdnsjcs",
+        },
+        {
+          nom: "lewis",
+          url: img,
+          nom: "jin",
+          email: "jin@gmail.com",
+          poste: "ifhsj",
+          numero: '0745316454',
+          link: "jdnsjcs",
+        },
+      ],
+    };
+  },
+};
 </script>
-<style lang="">
+<style lang="css" scoper>
+.sep{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
 </style>
